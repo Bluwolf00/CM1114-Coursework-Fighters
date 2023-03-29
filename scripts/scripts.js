e625b11d967be10f7f9519b2d2e80e7e7da02f69
Bluwolf00 <62963173+Bluwolf00@users.noreply.github.com>
2023-03-29 19:35:17 +0100

    Created Scoreboard Page & More Bootstrap Stuff
    + Created Scoreboard Page
    + Reworked the Calendar to use the Bootstrap table.
    + Other things

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,20 @@
+/* Define login script that prompts user when username and PW is entered */
+
 var counter = 2;
-function login() {
+function login1() {
     if (counter % 2 == 0) {
+        document.getElementById("loginB").innerHTML = "Log Out";
         alert('You have been logged in!');
     } else {
+        document.getElementById("loginB").innerHTML = "Log In";
         alert('You have been logged out!');
     }
     counter += 1;
 }
 
+/* Function for prompting user that form has been submitted */
 function send_form(id,content) {
     var contain = document.getElementById(id);
     contain.innerHTML = content;
+    contain.hidden = false;
 }
